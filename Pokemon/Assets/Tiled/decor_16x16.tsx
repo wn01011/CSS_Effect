4fd0de52957548b19125df4dcc6f631373ffdccb
wn01011 <wn01011@naver.com>
2022-09-13 08:50:38 +0900

    pokemon

---
+++ Pokemon/Assets/Tiled/decor_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="decor_16x16" tilewidth="16" tileheight="16" tilecount="20" columns="4">
+ <image source="../../../../mystic_woods_free_v0.2/sprites/tilesets/decor_16x16.png" width="64" height="80"/>
+</tileset>
