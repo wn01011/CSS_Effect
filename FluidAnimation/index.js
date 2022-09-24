a492a1fd8ec2f937ca311c60bb6705673c445eff
Naron <naron@111percent.net>
2022-09-24 10:41:04 +0900

    grass + tree

--- FluidAnimation/index.js
+++ FluidAnimation/index.js
@@ -7,42 +7,68 @@ const ctx = canvas.getContext(`2d`);
 let tempStartX = canvas.width / 2;
 let tempStartY = canvas.height / 2 - 80;
 function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
-  console.log(startX, startY);
   ctx.beginPath();
   ctx.save();
   ctx.strokeStyle = color1;
   ctx.fillStyle = color2;
+  ctx.shadowBlur = 15;
+  ctx.shadowColor = `black`;
+  ctx.lineWidth = branchWidth;
   ctx.translate(startX, startY);
   ctx.rotate(angle * (Math.PI / 180));
   ctx.moveTo(0, 0);
-  ctx.lineTo(0, -len);
+  // ctx.lineTo(0, -len);
+
+  if (angle > 0) {
+    ctx.bezierCurveTo(20, -len / 2, 20, -len / 2, 0, -len);
+  } else {
+    ctx.bezierCurveTo(20, -len / 2, -20, -len / 2, 0, -len);
+  }
   ctx.stroke();
 
-  if (len < 40) {
+  if (len < 30) {
     ctx.beginPath();
     ctx.arc(0, -len, 50, 0, Math.PI / 2);
     ctx.fill();
     ctx.restore();
     return;
   }
+
   let ranNum = 0;
   if (Math.random() < 0.9) {
-    ranNum = Math.random() * 15 + 5;
+    ranNum = Math.random() * 5 + 5;
     drawTree(0, -len, len * 0.85, angle + ranNum, branchWidth);
   }
   if (Math.random() < 0.9) {
-    ranNum = Math.random() * 15 + 5;
+    ranNum = Math.random() * 5 + 5;
     drawTree(0, -len, len * 0.85, angle - ranNum, branchWidth);
   }
   if (Math.random() < 0.2) {
-    ranNum = Math.random() * 15 - 7.5;
+    ranNum = Math.random() * 5 - 2.5;
     drawTree(0, -len, len * 0.5, angle + ranNum, branchWidth);
   }
   ctx.restore();
 }
-generateButton.onclick = () => {
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  setTimeout(() => {
-    drawTree(canvas.width / 2, canvas.height - 80, 120, 0, 2, `brown`, `green`);
-  }, 500);
-};
+// generateButton.onclick = () => {
+//   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+//   MakeTree(1);
+// };
+
+function MakeTree(len) {
+  for (let i = 0; i <= len; ++i) {
+    let branchWidth = Math.random() * 20 + 1;
+    drawTree(
+      (canvas.width * (i + 1)) / (len + 2),
+      canvas.height - 80,
+      160,
+      0,
+      branchWidth,
+      `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
+        Math.random() * 255
+      }, 0.4)`,
+      `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
+        Math.random() * 255
+      }, 1)`
+    );
+  }
+}
